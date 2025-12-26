edb6e2bfd5f33da47509b2d5df95c86384a66e28
bhautiknayee <nayeebhautik@gmail.com>
2025-12-26 15:43:12 -0500

    First commit

---
+++ src/main/java/com/jobfit/resume_engine/ResumeEngineApplication.java
@@ -0,0 +1,13 @@
+package com.jobfit.resume_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobfit/resume_engine/ResumeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobfit.resume_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
